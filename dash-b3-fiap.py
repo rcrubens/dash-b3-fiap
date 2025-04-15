80096507025e83d0463346fc8b8ab46872a5e49f
Rubens Campos <rcrubens@live.com>
2025-04-15 20:36:53 -0300

    Mais uma tentativa

--- dash-b3-fiap.py
+++ dash-b3-fiap.py
@@ -15,18 +15,18 @@ ticker = st.text_input('Digite o ticker da ação', 'BBAS3')
 empresa = yf.Ticker(f"{ticker}.SA")
 
 tickerDF = empresa.history(
-    period = "1d",
-    start = "2019-01-01",
-    end = "2025-04-10"
+    period="1d",
+    start="2019-01-01",
+    end="2025-04-10"
 )
 
 col1, col2,col3 = st.columns([1, 1, 1])
 with col1:
-    st.write(f"**Empresa:**{empresa.info['longName']}")
+    st.write(f"**Empresa:** {empresa.info['longName']}")
 with col2:
-    st.write(f"**Mercado:**{empresa.info['industry']}")
+    st.write(f"**Mercado:** {empresa.info['industry']}")
 with col3:
-    st.write(f"**Preço Atual:**{empresa.info['currentPrice']}")
+    st.write(f"**Preço Atual:** {empresa.info['currentPrice']}")
 
 st.line_chart(tickerDF.Close)
 st.bar_chart(tickerDF.Dividends)
