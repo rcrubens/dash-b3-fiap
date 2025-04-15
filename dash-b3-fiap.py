58ddb8a838dc95aeb41768e54a7f7d6ae1eedeb4
Rubens Campos <rcrubens@live.com>
2025-04-15 20:33:43 -0300

    Eliminando espaços pra ver se agora vai

--- dash-b3-fiap.py
+++ dash-b3-fiap.py
@@ -22,11 +22,11 @@ tickerDF = empresa.history(
 
 col1, col2,col3 = st.columns([1, 1, 1])
 with col1:
-    st.write(f"**Empresa:** {empresa.info['longName']}")
+    st.write(f"**Empresa:**{empresa.info['longName']}")
 with col2:
-    st.write(f"**Mercado:** {empresa.info['industry']}")
+    st.write(f"**Mercado:**{empresa.info['industry']}")
 with col3:
-    st.write(f"**Preço Atual:** {empresa.info['currentPrice']}")
+    st.write(f"**Preço Atual:**{empresa.info['currentPrice']}")
 
 st.line_chart(tickerDF.Close)
 st.bar_chart(tickerDF.Dividends)
