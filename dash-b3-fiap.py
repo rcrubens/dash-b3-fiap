1dfe2ce9dad74984081c26c67e64c9091aee1382
Rubens Campos <rcrubens@live.com>
2025-04-16 00:13:52 -0300

    versão final

--- dash-b3-fiap.py
+++ dash-b3-fiap.py
@@ -11,7 +11,7 @@ st.set_page_config(
 
 st.header("**PAINEL DE PREÇOS DE FECHAMENTO E DIVIDENDOS DE AÇÕES DA B3**")
 
-ticker = st.text_input('Digite o ticker da ação','BBAS3') # sem espaço depois da vírgula
+ticker = st.text_input('Digite o ticker da ação', 'BBAS3')
 empresa = yf.Ticker(f"{ticker}.SA")
 
 tickerDF = empresa.history(
@@ -22,11 +22,11 @@ tickerDF = empresa.history(
 
 col1, col2, col3 = st.columns([1, 1, 1])
 with col1:
-    st.write(f"**Empresa:**' {empresa.info['longName']}")
+    st.write(f"**Empresa:** {empresa.info['longName']}")
 with col2:
-    st.write(f"**Mercado:**' {empresa.info['industry']}")
+    st.write(f"**Mercado:** {empresa.info['industry']}")
 with col3:
-    st.write(f"**Preço Atual:**' {empresa.info['currentPrice']}")
+    st.write(f"**Preço Atual:** {empresa.info['currentPrice']}")
 
 st.line_chart(tickerDF.Close)
 st.bar_chart(tickerDF.Dividends)
